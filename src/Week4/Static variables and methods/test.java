11038db0bbf3e021b621717f851e58957145e656
adeahmed <98258192+adeahmed@users.noreply.github.com>
2023-09-16 17:56:32 +0300

    uuss

--- src/Week4/Static variables and methods/test.java
+++ src/Week4/Static variables and methods/test.java
@@ -1,38 +1,22 @@
-// package Week4;
+package
+public class Test{
+        try {
+        Ball ball = new Ball();
+        panel.add(ball);
 
-// public class test {
-//     private String name;
-//     private int age;
-
-// // medoton nimeäminen ja getteri ja setterin luominen!!
-//     public test(String name, int age){
-//         this.age = age;
-//         this.name = name;
-//     }
-
-
-//     public int getAge(){
-//         return age;
-//     }
-
-//     public void setAge(int age){
-//         this.age = age;
-//     }
+        for (int i = 1; i <= STEPS; i++) {
+            ball.move(panel.getBounds());
+            panel.paint(panel.getGraphics());  // (!!)
+            Thread.sleep(DELAY);
+        }
+    }
+    catch (InterruptedException e)
+    { }
 
 
-// }
 
- public class test {
-     int omaLuku = 5;
-     public int lisaaLuku(int luku, int luku2){
-        return luku + luku2;
-     }
-     pubic static void main(String[] args){
-         test oma = new test();
-         int luku = 5;
-         int luku2 = 10;
-         int omaLuku = lisaaLuku(luku, luku2);
-         System.out.println(omaLuku);
-     }
-    
- }
+        try {
+            Ball ball = new Ball();
+            panel
+    }
+}
